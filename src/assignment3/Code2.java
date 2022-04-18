da376c1d79d21572b5a692504dc9ddb28061a9bb
Dhruv Prajapati <Dhruv.Prajapati@einfochips.com>
2022-04-18 21:39:40 +0530

    commited changes

--- src/assignment3/Code2.java
+++ src/assignment3/Code2.java
@@ -3,9 +3,7 @@
 
 package assignment3;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
@@ -17,7 +15,7 @@ public class Code2 {
 		File replaceFile = new File("replaceFile.txt");
 		if(replaceFile.exists())
 		{
-			System.out.println("numbers file already exists");
+			System.out.println("file already exists");
 		}
 		else
 		{
@@ -25,21 +23,29 @@ public class Code2 {
 		}
 		
 		try (FileWriter fw = new FileWriter("replaceFile.txt")) {
-			fw.write("hello dhruv prajapati\n");
-			fw.write("hello dhruv prajapati\n");
-			fw.write("hello dhruv dhruv\n");
-			fw.write("hello dhruv prajapati\n");
-			fw.write("hello dhruv prajapati\n");
+			fw.write("A paragraph is a collection of words strung together to make a longer\n"
+					+ "unit than a sentence. Several sentences often make to a paragraph . \n"
+					+ "There are normally three to eight sentences in a paragraph . \n"
+					+ "paragraph can start with a five-space indentation or by skipping a line\n"
+					+ "and then starting over.\n");
+			fw.write("paragraph dhruv prajapati\n");
+			fw.write("hello welcome to the java\n");
+			fw.write("welcome to my paragraph\n");
 		}
-		Scanner file = new Scanner(replaceFile);
+		Scanner fileData = new Scanner(replaceFile);
+		
 		int count = 0;
 		String word = "";
-		while(file.hasNext())
-		{
-			word = word +  file.next() + " ";
-		}
 		
+		while(fileData.hasNext())
+		{
+			word = word +  fileData.next() + " ";
+		}
+		fileData.close();
+		
+		System.out.println("Before replacing file contents: ");
 		System.out.println(word);
+		System.out.println();
 		
 		Scanner string = new Scanner(word);
 		String temp;
@@ -47,19 +53,23 @@ public class Code2 {
 		{
 			temp = string.next();
 
-			if(temp.equals("dhruv"))
+			if(temp.equals("paragraph"))
 			{
-				System.out.println(temp+" ");
+//				System.out.println(temp+" ");
 				count++;
 			}
 		}
-		word = word.replace("dhruv", "abc");
-			
-		System.out.println("Count: " + count);
+		string.close();
+		word = word.replace("paragraph", "abc");
+		System.out.println("Replacing 'paragraph' by 'abc'");
+		System.out.println("No of replacement of: " + count);
+		System.out.println();
 		
 		FileWriter fw = new FileWriter("replaceFile.txt");
 		fw.write(word);
 		fw.close();
+		
+		System.out.println("After replacing file contents: ");
 		System.out.println(word);
 		}
 	
